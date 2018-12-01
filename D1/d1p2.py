2a4fb03f6651d81bfec574a8250504c83bd7819d
Ferran Espigares <github@espigares.es>
2018-12-01 15:57:17 +0100

    Day1Part2 complete

--- D1/d1p2.py
+++ D1/d1p2.py
@@ -1,25 +1,20 @@
 from __future__ import print_function
-with open("input.txt") as f:
+with open("c:\\users\\user\\documents\\dev\\adventofcode2018\\D1\\input.txt") as f:
     lines = f.readlines()
 freq=0
-freq_h=[0]
+freq_h={}
 lines_len=len(lines)
 i=0
 print("GO!")
 while True:
     freq+=int(lines[i])
-    if freq in freq_h:
+    if freq_h.get(freq,False):
         break
-    freq_h.append(freq)
+    else:
+        freq_h[freq]=True
     if i==(lines_len-1):
         i=0
         print(".",end='')
     else:
         i+=1
-
-# for line in lines:
-#    freq+=int(line)
-#    if freq in freq_h:
-#        break
-#    freq_h.append(freq)
 print(freq)
