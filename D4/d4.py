bc428443edf8e3c52708890872f2aca4dab23d7e
Ferran Espigares <github@espigares.es>
2018-12-10 07:36:37 +0100

    WIP on Day4

--- D4/d4.py
+++ D4/d4.py
@@ -4,8 +4,16 @@ import numpy as np
 with open("c:\\users\\user\\documents\\dev\\adventofcode2018\\D3\\input.txt") as f:
     records = f.readlines()
 
+
+dtype=[('month',int),('day',int),('guardid',int)]    
+
 for record in records:
+    if re.search(r".*falls.*",record) is not None:
+
+        continue
+    if re.search(r".*wakes.*",record) is not None:
+        continue
+    
     match=re.search(r"\[(\d+)\-(\d+)\-(\d+) (\d+):(\d+)\] Guard #(\d+) begins shift",record) #parse claim string
     year,month,day,hour,minute,id=match.group(1,2,3,4,5,6) #map strings to ints
 
-dtype=[('mont',int),('day',int),('guardid',int)]    
